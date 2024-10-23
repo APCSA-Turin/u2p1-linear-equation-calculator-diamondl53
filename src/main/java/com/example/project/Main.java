f89517ff63e7fc069d24faf0548f7e8f936bfb62
diamondl53 <diamondl53@nycstudents.net>
2024-10-23 05:12:30 +0000

    commit

---
+++ src/main/java/com/example/project/Main.java
@@ -0,0 +1,12 @@
+package com.example.project;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    LinearCalculator c = new LinearCalculator("(-3,-1)","(4,-3)");
+        c.slope();
+    }
+    
+}
